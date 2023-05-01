7285a31ea83a6f85fb1193fae7353134b4e14c28
Maxim Frolov <buterbrodhs@gmail.com>
2023-05-02 00:25:39 +0300

    test config

--- frontend/src/pages-layers/home/ui/home.tsx
+++ frontend/src/pages-layers/home/ui/home.tsx
@@ -1,7 +1,9 @@
 import { Button } from 'shared/ui/button';
 import { Page } from 'widgets/page';
+import { config } from 'shared/lib/config';
 
 export const Home = () => {
+  console.log('config', config);
   return (
     <Page context={{ user: null }}>
       <div>
--- frontend/src/shared/lib/config.ts
+++ frontend/src/shared/lib/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  WEATHER_API_HOST: '',
-  WEATHER_API_TOKEN: ''
+  WEATHER_API_HOST: process.env.NEXT_PUBLIC_WEATHER_API_HOST || '',
+  WEATHER_API_TOKEN: process.env.NEXT_PUBLIC_GISMETEO_TOKEN || ''
 };
